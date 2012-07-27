723c40305921d11ea5dac6f19b526bfde677b453
Alon Zakai <alonzakai@gmail.com>
2012-07-27 14:17:49 -0700

    adjust stack alignment

--- lib/Target/DCPU16/DCPU16FrameLowering.h
+++ lib/Target/DCPU16/DCPU16FrameLowering.h
@@ -27,7 +27,7 @@ protected:
 
 public:
   explicit DCPU16FrameLowering(const DCPU16Subtarget &sti)
-    : TargetFrameLowering(TargetFrameLowering::StackGrowsDown, 1, -1), STI(sti) {
+    : TargetFrameLowering(TargetFrameLowering::StackGrowsDown, 8, 0), STI(sti) {
   }
 
   /// emitProlog/emitEpilog - These methods insert prolog and epilog code into
